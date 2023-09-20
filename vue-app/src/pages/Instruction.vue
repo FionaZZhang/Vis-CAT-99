58839c914c93b7053c95a1e1b28f07a2f14e1003
ZHEQI SHEN <>
2023-09-20 17:36:11 +1000

    Try to enable scroll the screen, half way through

--- vue-app/src/pages/Instruction.vue
+++ vue-app/src/pages/Instruction.vue
@@ -73,7 +73,7 @@
       <div class="instructionPopUp-modal">
         <div class="inter_page_content">
           <img class="instructionGIF" src="../assets/copyPattern.gif" alt="instructionGIF">
-          <img class="instructionConfirm" id="buttonInstructionConfirm" src="../assets/button_confirm.png" @click="CloseInstruction(); loadPatternAndConnect(this.originalPattern); startTimer()">
+          <img class="instructionConfirm" id="buttonInstructionConfirm" src="../assets/button_confirm.png" @click="CloseInstruction(); startTimer()">
         </div>   
       </div>
     </div>
@@ -105,9 +105,14 @@ export default defineComponent({
     };
   },
   mounted() {
+    // clear all the existing lines;
     this.StartInstruction();
+    this.loadPatternAndConnect(this.originalPattern);
+    this.ReallignCells();
     this.svg = this.$el.querySelector('.connector');
     document.addEventListener('touchmove', this.preventScroll, { passive: false });
+    window.addEventListener('scroll', this.ReallignCells);
+    window.addEventListener('resize', this.ReallignCells);
     // this.loadPatternAndConnect(this.originalPattern);
   },
   beforeUnmount() {
@@ -209,6 +214,7 @@ export default defineComponent({
         }
       }
     },
+
     handleMouseOver(event) {
       const cell = event.target;
       const id = cell.dataset.id;
@@ -222,7 +228,12 @@ export default defineComponent({
           this.drawLine(prevCell, cell);
         }
       }
+
+      if (this.pattern.length > 0){
+        this.ReallignCells();
+      }
     },
+
     handleTouchMove(event) {
       const touch = event.touches[0];
       const element = document.elementFromPoint(touch.clientX, touch.clientY);
@@ -239,7 +250,12 @@ export default defineComponent({
           }
         }
       }
+
+      if (this.pattern.length > 0){
+        this.ReallignCells();
+      }
     },
+
     drawLine(cell1, cell2) {
       const rect1 = cell1.getBoundingClientRect();
       const rect2 = cell2.getBoundingClientRect();
@@ -252,6 +268,7 @@ export default defineComponent({
       line.setAttribute('stroke-width', '5');
       this.svg.appendChild(line);
     },
+
     loadPatternAndConnect(patternDots) {
       const dots = document.querySelectorAll('.dot');
       const svg = document.querySelector('.connector');
@@ -297,6 +314,54 @@ export default defineComponent({
         }
       }
     },
+
+    
+    RedrawPatternWhenScroll(patternDots) {
+      const dots = document.querySelectorAll('.cell');
+      const svg = document.querySelector('.connector');
+
+      for (const dot of dots) {
+        const dotId = parseInt(dot.dataset.id);
+        if (patternDots.includes(dotId)) {
+          dot.classList.add('active');
+        }
+      }
+
+      for (let i = 0; i < patternDots.length - 1; i++) {
+        const dotId1 = patternDots[i];
+        const dotId2 = patternDots[i + 1];
+        const dot1 = document.querySelector(`.cell[data-id="${dotId1}"]`);
+        const dot2 = document.querySelector(`.cell[data-id="${dotId2}"]`);
+
+        const x1 = dot1.offsetLeft + dot1.offsetWidth / 2;
+        const y1 = dot1.offsetTop + dot1.offsetHeight / 2;
+        const x2 = dot2.offsetLeft + dot2.offsetWidth / 2;
+        const y2 = dot2.offsetTop + dot2.offsetHeight / 2;
+
+        const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+        line.setAttribute('x1', x1);
+        line.setAttribute('y1', y1);
+        line.setAttribute('x2', x2);
+        line.setAttribute('y2', y2);
+        line.setAttribute('stroke', '#3498db');
+        line.setAttribute('stroke-width', '5');
+        svg.appendChild(line);
+
+        // if (i === patternDots.length - 2) {
+        //   // Calculate the angle of the line segment and reverse it
+        //   const angle = Math.atan2(y2 - y1, x2 - x1) - Math.PI;
+        //   // Rest of the code remains the same
+        //   const arrowSize = 40; // Adjust the size of the arrowhead
+        //   const arrowX = x2 - arrowSize * Math.cos(angle);
+        //   const arrowY = y2 - arrowSize * Math.sin(angle);
+        //   const arrow = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
+        //   arrow.setAttribute('points', `${arrowX},${arrowY} ${(arrowX + arrowSize * Math.cos(angle - Math.PI / 6))},${(arrowY + arrowSize * Math.sin(angle - Math.PI / 6))} ${(arrowX + arrowSize * Math.cos(angle + Math.PI / 6))},${(arrowY + arrowSize * Math.sin(angle + Math.PI / 6))}`);
+        //   arrow.setAttribute('fill', 'black'); // Arrowhead color
+        //   svg.appendChild(arrow);
+        // }
+      }
+    },
+
     endDrawing() {
       this.isDrawing = false;
     },
@@ -310,6 +375,21 @@ export default defineComponent({
         this.svg.removeChild(this.svg.lastChild);
       }
     },
+
+    ReallignCells() {
+      if (this.pattern.length > 0){
+        // const cells = this.$el.querySelectorAll('.cell.active');
+        // cells.forEach(cell => {
+        // cell.classList.remove('active');
+        // });
+        while (this.svg.childElementCount > 0) {
+          this.svg.removeChild(this.svg.lastChild);
+        }
+        this.loadPatternAndConnect(this.originalPattern);
+        this.RedrawPatternWhenScroll(this.pattern);
+      }   
+    },
+
     revertPattern() {
       if (this.pattern.length === 0) return;
       // Remove the last item from the pattern arrays
