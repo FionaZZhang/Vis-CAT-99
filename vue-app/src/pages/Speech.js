e0aa3751dadfb770f758519260730a43854cd9bf
am-yang <118100011+am-yang@users.noreply.github.com>
2023-09-20 11:30:42 +1000

    Fixed audio playing bug

--- vue-app/src/pages/Speech.js
+++ vue-app/src/pages/Speech.js
@@ -3,6 +3,8 @@ export let voiceOn = true;
 export const voices = ['anita'];
 export let currentVoice = '';
 
+let audio = null;
+
 // export function speak(readText){
 //     speech.text = readText;
 //     speech.voice = currentVoice_2;
@@ -24,13 +26,22 @@ export function getVoices(){
 }
 
 export function speak(file){
+    pauseAudio();
     if (voiceOn){
         // If no voice selected, set first one as default 
         if (currentVoice === ''){
             currentVoice = voices[0];
         }
         let sound = require('@/assets/audio/' + currentVoice + '/' + file + '.mp3');
-        let audio = new Audio(sound);
+        audio = new Audio(sound);
         audio.play();
+    }
+}
+
+function pauseAudio() {
+    // If audio is not undefined and if is playing, pause it
+    if (audio && !audio.paused) {
+      audio.pause();
+      audio.currentTime = 0; // Rewind track to beginning (is you need this)
     }
 }
